e3f1674eee6b41cf5e7e8a158705e03a97971d05
John Jordan Shelley <jshelley553@gmail.com>
2019-06-08 19:35:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,13 @@
 def current_player(board)
   counter = 0
   board.each do |ele|
-    if ele.nil? == false 
-      counter += 1 
-    end 
-    if counter = 1 
+    if ele.nil? == false
+      counter += 1
+    end
+    if counter = 1
       return "X"
-    else 
+    else
       return "O"
-    end 
+    end
   end
-end 
-
+end
